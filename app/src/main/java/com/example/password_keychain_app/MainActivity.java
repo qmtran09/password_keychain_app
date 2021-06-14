e4809bc0d4c0e9881d08e0defc3ec1dddc83cb27
Quang Tran <qmtran09@gmail.com>
2021-06-14 20:05:52 +0700

    Setup initial pk

--- app/src/main/java/com/example/password_keychain_app/MainActivity.java
+++ app/src/main/java/com/example/password_keychain_app/MainActivity.java
@@ -1,19 +1,106 @@
 package com.example.password_keychain_app;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+
+import java.util.Base64;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView banner;
+    private String uID;
+    private DatabaseReference db;
+    private User u;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = FirebaseDatabase.getInstance("https://passwordkeychain-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference();
+
+        banner = (TextView) findViewById(R.id.banner);
+
+
+
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            uID = user.getUid();
+
+        }else{
+            startActivity(new Intent(this,Login.class));
+        }
+
+        db.child("Users").child(uID).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull  Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                    u = task.getResult().getValue(User.class);
+                    banner.setText("Hi "+u.name+"!");
+                }
+            }
+        });
+
+        //if first time user generate public and private key
+        if(u.pk==null){
+            createKeys genKeys = new createKeys();
+            String publicKey = Base64.getEncoder().encodeToString(genKeys.getPublicKey().getEncoded());
+            String privatekey = Base64.getEncoder().encodeToString(genKeys.getPrivateKey().getEncoded());
+
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setCancelable(true);
+            builder.setTitle("This is your private key, please store securely. This key will not be shown again!");
+            builder.setMessage(privatekey);
+
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+
+
+        }
+
+
+
+
+
+
+
 
 
     }
--- app/src/main/java/com/example/password_keychain_app/User.java
+++ app/src/main/java/com/example/password_keychain_app/User.java
@@ -1,13 +1,24 @@
 package com.example.password_keychain_app;
 
 public class User {
-    public String name,email;
+    public String name,email,pk;
 
     public User(){
 
     }
-    public User(String name, String email){
+    public User(String name, String email,String pk){
         this.name = name;
         this.email = email;
+        this.pk = pk;
+
     }
+
+    public void setPk(String publickey){
+        this.pk = publickey;
+    }
+    public String getPk(){
+        return this.pk;
+    }
+
+
 }
